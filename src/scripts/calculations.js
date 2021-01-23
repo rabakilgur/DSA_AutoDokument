aa1eca27e33e3bd46ae693aa6d92ce26adcfd7a9
Vanillequark <joe-thao@hotmail.de>
2021-01-23 18:06:18 +0100

    Incomplete base-value calculation

--- src/scripts/calculations.js
+++ src/scripts/calculations.js
@@ -36,9 +36,39 @@ function calc_dependency(target_name, calc_string) {
 }
 
 $(document).ready(() => {
-	// Page 1 calculations:
-	add_calc_dependency('LeP-Start', '({Konstitution-Start} + {Konstitution-Start} + {Körperkraft-Start}) / 2 + {LeP-Mod}');
+	/***********************************/
+	/**********  BASIS-WERTE  **********/
+	/***********************************/
+
+	/******** Basis-Werte Start ********/
+	add_calc_dependency('LeP-Start', '(2 * {Konstitution-Start} + {Körperkraft-Start}) / 2 + {LeP-Mod}');
 	add_calc_dependency('AuP-Start', '({Mut-Start} + {Konstitution-Start} + {Gewandheit-Start}) / 2 + {AuP-Mod}');
+	add_calc_dependency('MR-Start', '({Mut-Start} + {Klugheit-Start} + {Konstitution-Start}) / 5 + {MR-Mod}');
+	add_calc_dependency('INI-Start', '(2 * {Mut-Start} + {Intuition-Start} + {Gewandheit-Start}) / 5 + {INI-Mod}');
+	add_calc_dependency('AT-Start', '({Mut-Start} + {Gewandheit-Start} + {Körperkraft-Start}) / 5 + {AT-Mod}');
+	add_calc_dependency('PA-Start', '({Intuition-Start} + {Gewandheit-Start} + {Körperkraft-Start}) / 5 + {PA-Mod}');
+	add_calc_dependency('FK-Start', '({Intuition-Start} + {Fingerfertigkeit-Start} + {Körperkraft-Start}) / 5 + {FK-Mod}');
+
+	/******** Basis-Werte Aktuell ********/
+	add_calc_dependency('LeP-Aktuell', '(2 * {Konstitution-Aktuell} + {Körperkraft-Aktuell}) / 2 + {LeP-Mod}');
+	add_calc_dependency('AuP-Aktuell', '({Mut-Aktuell} + {Konstitution-Aktuell} + {Gewandheit-Aktuell}) / 2 + {AuP-Mod}');
+	add_calc_dependency('MR-Aktuell', '({Mut-Aktuell} + {Klugheit-Aktuell} + {Konstitution-Aktuell}) / 5 + {MR-Mod}');
+	add_calc_dependency('INI-Aktuell', '(2 * {Mut-Aktuell} + {Intuition-Aktuell} + {Gewandheit-Aktuell}) / 5 + {INI-Mod}');
+	add_calc_dependency('AT-Aktuell', '({Mut-Aktuell} + {Gewandheit-Aktuell} + {Körperkraft-Aktuell}) / 5 + {AT-Mod}');
+	add_calc_dependency('PA-Aktuell', '({Intuition-Aktuell} + {Gewandheit-Aktuell} + {Körperkraft-Aktuell}) / 5 + {PA-Mod}');
+	add_calc_dependency('FK-Aktuell', '({Intuition-Aktuell} + {Fingerfertigkeit-Aktuell} + {Körperkraft-Aktuell}) / 5 + {FK-Mod}');
+
+	/******** Basis-Werte max. Zukauf ********/
+	// add_calc_dependency('TODO', '{Konstitution-Aktuell} / 2'); // TODO: LeP max Zukauf
+	// add_calc_dependency('TODO', '{Konstitution-Aktuell}'); // TODO: AuP max Zukauf
+	// add_calc_dependency('TODO', '{Mut-Aktuell} / 2'); // TODO: MR max Zukauf
+
+	/******** Wundschwelle ********/
+	// add_calc_dependency('TODO', '{Konstitution-Aktuell} / 2'); // TODO: 1. Wundschwelle
+	// add_calc_dependency('TODO', '{Konstitution-Aktuell}'); // TODO: 2. Wundschwelle
+	// add_calc_dependency('TODO', '3 * {Konstitution-Aktuell} / 2'); // TODO: 3. Wundschwelle
+	// add_calc_dependency('TODO', '2 * {Konstitution-Aktuell}'); // TODO: 4. Wundschwelle
+
 	// ToDo: Add more calculations
 
 });
