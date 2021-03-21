76861179f71dae88dfd8fd9a6cf91e2c4a68716b
Robin Garbe <mail@robin-garbe.de>
2021-03-21 21:17:48 +0100

    Updated the document - Added some new preset images - Fixed some class names - fixed a bug in automatic calculations

--- src/scripts/calculations.js
+++ src/scripts/calculations.js
@@ -20,7 +20,7 @@ function calc_dependency(target_name, calc_string) {
 	const $target = $('.Calc-' + target_name);
 
 	calc = calc_string.replace(/{(\w|\d|[äöüß-])+}/g, item => {
-		const $field = $('.Edit-' + item.substr(1, item.length - 2));
+		const $field = $('.Edit-' + item.substr(1, item.length - 2)).first();
 		return ($field.text() === '') ? "0" : $field.text();
 	});
 	const result = isNaN(eval(calc)) ? "X" : Math.round(eval(calc));
@@ -41,37 +41,37 @@ $(document).ready(() => {
 	/***********************************/
 
 	/******** Basis-Werte Start ********/
-	add_calc_dependency('LeP-Start', '(2 * {Konstitution-Start} + {Körperkraft-Start}) / 2 + {LeP-Mod}');
-	add_calc_dependency('AuP-Start', '({Mut-Start} + {Konstitution-Start} + {Gewandheit-Start}) / 2 + {AuP-Mod}');
-	// add_calc_dependency('AsP-Start', '({Mut-Start} + {Intuition-Start} + {Charisma-Start}) / 2 + {AsP-Mod}'); // TODO: nur bei Zauberer
+	add_calc_dependency('LeP-Start', '(2 * {KO-Start} + {KK-Start}) / 2 + {LeP-Mod}');
+	add_calc_dependency('AuP-Start', '({MU-Start} + {KO-Start} + {GE-Start}) / 2 + {AuP-Mod}');
+	// add_calc_dependency('AsP-Start', '({MU-Start} + {IN-Start} + {CH-Start}) / 2 + {AsP-Mod}'); // TODO: nur bei Zauberer
 	// add_calc_dependency('KaP-Start', 'TODO'); // TODO: nur bei Geweihten
-	add_calc_dependency('MR-Start', '({Mut-Start} + {Klugheit-Start} + {Konstitution-Start}) / 5 + {MR-Mod}');
-	add_calc_dependency('INI-Start', '(2 * {Mut-Start} + {Intuition-Start} + {Gewandheit-Start}) / 5 + {INI-Mod}');
-	add_calc_dependency('AT-Start', '({Mut-Start} + {Gewandheit-Start} + {Körperkraft-Start}) / 5 + {AT-Mod}');
-	add_calc_dependency('PA-Start', '({Intuition-Start} + {Gewandheit-Start} + {Körperkraft-Start}) / 5 + {PA-Mod}');
-	add_calc_dependency('FK-Start', '({Intuition-Start} + {Fingerfertigkeit-Start} + {Körperkraft-Start}) / 5 + {FK-Mod}');
+	add_calc_dependency('MR-Start', '({MU-Start} + {KL-Start} + {KO-Start}) / 5 + {MR-Mod}');
+	add_calc_dependency('INI-Start', '(2 * {MU-Start} + {IN-Start} + {GE-Start}) / 5 + {INI-Mod}');
+	add_calc_dependency('AT-Start', '({MU-Start} + {GE-Start} + {KK-Start}) / 5 + {AT-Mod}');
+	add_calc_dependency('PA-Start', '({IN-Start} + {GE-Start} + {KK-Start}) / 5 + {PA-Mod}');
+	add_calc_dependency('FK-Start', '({IN-Start} + {FF-Start} + {KK-Start}) / 5 + {FK-Mod}');
 
 	/******** Basis-Werte Aktuell ********/
-	add_calc_dependency('LeP-Aktuell', '(2 * {Konstitution-Aktuell} + {Körperkraft-Aktuell}) / 2 + {LeP-Mod} + {LeP-Zugekauft}');
-	add_calc_dependency('AuP-Aktuell', '({Mut-Aktuell} + {Konstitution-Aktuell} + {Gewandheit-Aktuell}) / 2 + {AuP-Mod} + {AuP-Zugekauft}');
-	// add_calc_dependency('AsP-Aktuell', '({Mut-Aktuell} + {Intuition-Aktuell} + {Charisma-Aktuell}) / 2 + {AsP-Mod} + {AsP-Zugekauft}'); // TODO: nur bei Zauberer
-	// add_calc_dependency('KaP-Aktuell', 'TODO'); // TODO: nur bei Geweihten
-	add_calc_dependency('MR-Aktuell', '({Mut-Aktuell} + {Klugheit-Aktuell} + {Konstitution-Aktuell}) / 5 + {MR-Mod}');
-	add_calc_dependency('INI-Aktuell', '(2 * {Mut-Aktuell} + {Intuition-Aktuell} + {Gewandheit-Aktuell}) / 5 + {INI-Mod}');
-	add_calc_dependency('AT-Aktuell', '({Mut-Aktuell} + {Gewandheit-Aktuell} + {Körperkraft-Aktuell}) / 5 + {AT-Mod}');
-	add_calc_dependency('PA-Aktuell', '({Intuition-Aktuell} + {Gewandheit-Aktuell} + {Körperkraft-Aktuell}) / 5 + {PA-Mod}');
-	add_calc_dependency('FK-Aktuell', '({Intuition-Aktuell} + {Fingerfertigkeit-Aktuell} + {Körperkraft-Aktuell}) / 5 + {FK-Mod}');
+	add_calc_dependency('LeP', '(2 * {KO} + {KK}) / 2 + {LeP-Mod} + {LeP-Zugekauft}');
+	add_calc_dependency('AuP', '({MU} + {KO} + {GE}) / 2 + {AuP-Mod} + {AuP-Zugekauft}');
+	// add_calc_dependency('AsP', '({MU} + {IN} + {CH}) / 2 + {AsP-Mod} + {AsP-Zugekauft}'); // TODO: nur bei Zauberer
+	// add_calc_dependency('KaP', 'TODO'); // TODO: nur bei Geweihten
+	add_calc_dependency('MR', '({MU} + {KL} + {KO}) / 5 + {MR-Mod}');
+	add_calc_dependency('INI', '(2 * {MU} + {IN} + {GE}) / 5 + {INI-Mod}');
+	add_calc_dependency('AT', '({MU} + {GE} + {KK}) / 5 + {AT-Mod}');
+	add_calc_dependency('PA', '({IN} + {GE} + {KK}) / 5 + {PA-Mod}');
+	add_calc_dependency('FK', '({IN} + {FF} + {KK}) / 5 + {FK-Mod}');
 
 	/******** Basis-Werte max. Zukauf ********/
-	// add_calc_dependency('TODO', '{Konstitution-Aktuell} / 2'); // TODO: LeP max Zukauf
-	// add_calc_dependency('TODO', '{Konstitution-Aktuell}'); // TODO: AuP max Zukauf
-	// add_calc_dependency('TODO', '{Mut-Aktuell} / 2'); // TODO: MR max Zukauf
+	// add_calc_dependency('TODO', '{KO} / 2'); // TODO: LeP max Zukauf
+	// add_calc_dependency('TODO', '{KO}'); // TODO: AuP max Zukauf
+	// add_calc_dependency('TODO', '{MU} / 2'); // TODO: MR max Zukauf
 
 	/******** Wundschwelle ********/
-	// add_calc_dependency('TODO', '{Konstitution-Aktuell} / 2'); // TODO: 1. Wundschwelle
-	// add_calc_dependency('TODO', '{Konstitution-Aktuell}'); // TODO: 2. Wundschwelle
-	// add_calc_dependency('TODO', '3 * {Konstitution-Aktuell} / 2'); // TODO: 3. Wundschwelle
-	// add_calc_dependency('TODO', '2 * {Konstitution-Aktuell}'); // TODO: 4. Wundschwelle
+	// add_calc_dependency('TODO', '{KO} / 2'); // TODO: 1. Wundschwelle
+	add_calc_dependency('WS-2', '{KO} + ({WS} - Math.ceil({KO} / 2))'); // 2. Wundschwelle
+	add_calc_dependency('WS-3', '1.5 * {KO} + ({WS} - Math.ceil({KO} / 2))'); // 3. Wundschwelle
+	// add_calc_dependency('TODO', '2 * {KO}'); // TODO: 4. Wundschwelle
 
 	// ToDo: Add more calculations
 
